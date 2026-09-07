b15bb430776f3a5aa3643fedcd6e1bfff1bae493
clakoxfiles <clakosasuke@gmail.com>
2026-09-07 16:00:43 +0300

    errors, indexes and sellers rev & sales count

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@
  * @param _product карточка товара
  * @returns {number}
  */
+
 function calculateSimpleRevenue(purchase, _product) {
    // @TODO: Расчет выручки от операции
+   const { discount, sale_price, quantity } = purchase
 }
 
 /**
@@ -15,8 +17,10 @@ function calculateSimpleRevenue(purchase, _product) {
  * @param seller карточка продавца
  * @returns {number}
  */
+
 function calculateBonusByProfit(index, total, seller) {
     // @TODO: Расчет бонуса от позиции в рейтинге
+    const { profit } = seller
 }
 
 /**
@@ -25,16 +29,53 @@ function calculateBonusByProfit(index, total, seller) {
  * @param options
  * @returns {{revenue, top_products, bonus, name, sales_count, profit, seller_id}[]}
  */
+
 function analyzeSalesData(data, options) {
-    // @TODO: Проверка входных данных
+    if (!data 
+        || !Array.isArray(data.sellers)
+        || data.sellers.length === 0
+        || !Array.isArray(data.products)
+        || data.products.length === 0
+        || !Array.isArray(data.purchase_records)
+        || data.purchase_records.length === 0
+    ) {
+        throw new Error('Некорректные входные данные')
+    }
 
-    // @TODO: Проверка наличия опций
+    const { calculateRevenue, calculateBonus } = options
 
-    // @TODO: Подготовка промежуточных данных для сбора статистики
+    if (calculateRevenue === undefined
+        || calculateBonus === undefined
+        || typeof calculateRevenue !== 'function'
+        || typeof calculateBonus !== 'function'
+    ) {
+        throw new Error('Чего то не хватает')
+    }
 
-    // @TODO: Индексация продавцов и товаров для быстрого доступа
+    const sellerStats = data.sellers.map(seller => ({
+        id: seller.id,
+        name: `${seller.first_name} ${seller.last_name}`,
+        revenue: 0,
+        profit: 0,
+        sales_count: 0,
+        products_sold: {}
+    }))
 
-    // @TODO: Расчет выручки и прибыли для каждого продавца
+    const sellerIndex = Object.fromEntries(sellerStats.map(arr => [arr.id, arr]))
+    const productIndex = Object.fromEntries(data.products.map(arr => [arr.sku, arr]))
+
+    data.purchase_records.forEach(record => {
+        const seller = sellerIndex[record.seller_id]
+        seller.sales_count += 1
+        seller.revenue += record.total_amount
+
+        record.items.forEach(item => {
+            const product = productIndex[item.sku]
+            
+        })
+    })
+
+    console.log(sellerStats)
 
     // @TODO: Сортировка продавцов по прибыли
 
